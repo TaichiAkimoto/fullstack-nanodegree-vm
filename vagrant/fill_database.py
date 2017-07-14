23ee75d696481356e229ebd6056978f884773842
TaichiAkimoto <italyitalienitalia@gmail.com>
2017-07-14 16:25:14 +0900

    Add render_template to make rendering easier

--- vagrant/fill_database.py
+++ vagrant/fill_database.py
@@ -8,9 +8,31 @@ DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
 def main():
-    myFirstRestaurant = Restaurant(name = "Pizza Palace")
-    session.add(myFirstRestaurant)
-    session.commit()
+
+    # This is where the bulk operations will be done.
+    # s = Session()
+    # objects = [
+    # User(name="u1"),
+    # User(name="u2"),
+    # User(name="u3")
+    # ]
+    # s.bulk_save_objects(objects)
+    #objects1 = [
+    #    Restaurant(name='The CRUDdy Crab'),
+    #    Restaurant(name='Blue Burgers'),
+    #    Restaurant(name='Taco Hut'),
+    #]
+    #object2 = [
+        #MenuItem(name='Cheese Pizza',description='made with fresh cheese',price='$5.99',course='Entree'),
+        #MenuItem(name='Chocolate Cake',description='made with Dutch Chocolate',price='$3.99',course='Dessert'),
+        #MenuItem(name='Caesar Salad',description='with fresh organic vegetables',price='$5.99',course='Entree'),
+    #]
+    firstMenu = session.query(MenuItem).all()
+    for item in firstMenu:
+        print item.name
+        print item.course
+        print item.description
+        print item.id
 
 if __name__ == '__main__':
     main()
